cd714ac607df4c90427e755273e88e4aa9a1cbe3
SomeRandomPerson123 <walkerknapp3@gmail.com>
2015-05-14 19:15:27 -0400

    Fixed update link

--- src/main/java/com/AnZaNaMa/EnergyTools/Reference/VersioningNBT.java
+++ src/main/java/com/AnZaNaMa/EnergyTools/Reference/VersioningNBT.java
@@ -5,8 +5,8 @@ import net.minecraft.nbt.NBTTagCompound;
 public class VersioningNBT extends NBTTagCompound {
     public VersioningNBT(){
         this.setString("modDisplayName", Reference.MODNAME);
-        this.setString("newVersion", "1.0b1");
-        this.setString("updateUrl", "https://github.com/SomeRandomPerson9/EnergyTools/releases/download/1.0b1/EnergyTools-1.0.jar");
+        this.setString("newVersion", "1.8-1.0b1");
+        this.setString("updateUrl", "https://github.com/SomeRandomPerson9/EnergyTools/releases/download/1.8-1.0b1/EnergyTools-1.0.jar");
         this.setBoolean("isDirectLink", true);
     }
 }
